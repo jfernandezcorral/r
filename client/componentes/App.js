f71525613ee8d95514b2ed71e1c7d26e95e62e3d
Julio Ferndez <jfernandezcorral@gmail.com>
2017-08-29 19:19:31 +0200

    componente con scroll infinito

--- client/componentes/App.js
+++ client/componentes/App.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import estilos from'./App.scss';
 import Comercial from './comercial/Comercial';
+import ScrollInfinito from './base/ScrollInfinito';
 export default class App extends React.Component {
   render() {
-    /*return (
-     <div style={{textAlign: 'center'}} className={estilos.rojo}>
-        <h1>Hello World</h1>
-      </div>);*/
     return (
     	<div style={{height: '100%', display: 'flex'/*, alignItems: 'stretch'*/}}>
     		<aside style={{width: '30%', marginRight: '5px'}} className={estilos.panel}>
     			<Comercial/>
     		</aside>
     		<section style={{width: '70%'}} className={estilos.panel}>
-    			ghkfjkr
+    			<ScrollInfinito onTop={(s) => console.log(s)}>
+    				<div>
+    					<p>erewtrwerwer</p>
+    					<p>erewtrwerwer</p>
+    					<p>erewtrwerwer</p>
+    					<p>erewtrwerwer</p>
+    					<p>erewtrwerwer</p>
+    					<p>erewtrwerwer</p>
+    					<p>erewtrwerwer</p>
+    					<p>erewtrwerwer</p>
+    					<p>erewtrwerwer</p>
+    					<p>erewtrwerwer</p>
+    					<p>erewtrwerwer</p>
+    					<p>erewtrwerwer</p>
+    					<p>erewtrwerwer</p>
+    					<p>erewtrwerwer</p>
+    					<p>erewtrwerwer</p>
+    					<p>erewtrwerwer</p>
+    					<p>erewtrwerwer</p>
+    				</div>
+    			</ScrollInfinito>
     		</section>
     	</div>
     );
---
+++ client/componentes/base/ScrollInfinito.js
@@ -0,0 +1,52 @@
+import React, {Children} from 'react'
+import PropTypes from 'prop-types'
+export default class ScrollInfinito extends React.Component {
+	constructor(props) {
+    	super(props)
+    	this.scroll = null
+    	this.panel = null
+    	this.anteriorTop = 0
+    	this.handleScroll = this.handleScroll.bind(this)
+  	}
+  	render() {
+    	const {children, estilo} = this.props
+    	return (
+    		<div style={estilo} ref='s'>
+    			{Children.only(children)}
+    		</div>
+    	)
+  	}
+  	componentDidMount(){
+  		this.scroll = this.refs.s
+  		this.panel = this.scroll.firstChild
+  		this.scroll.addEventListener('scroll', this.handleScroll)
+  	}
+  	componentWillUnmount() {
+    	this.scroll.removeEventListener('scroll', this.handleScroll)
+  	}
+  	handleScroll() {
+  		if (Math.abs(this.anteriorTop-this.scroll.scrollTop)<2){
+  			return
+  		}
+  		else{
+  			this.anteriorTop = 0
+  		}
+  		if (this.panel.offsetHeight - this.scroll.offsetHeight <= this.scroll.scrollTop){
+  			this.props.onTop(this.scroll.scrollTop)
+  			this.anteriorTop = this.scroll.scrollTop
+  		}
+  	}
+}
+
+ScrollInfinito.propTypes = {
+ 	estilo: PropTypes.object,
+ 	children: PropTypes.element.isRequired,
+ 	onTop: PropTypes.func.isRequired
+}
+
+ScrollInfinito.defaultProps = {
+    estilo: {
+    	overflowY: 	'auto',
+    	height: 	"100%", 
+    }
+}
